fb791a24a05192372b7496734431499fb1d4d0f7
lathasrikakulapati <lathasribtech@gmail.com>
2025-02-11 10:35:04 +0530

    Update URL_Routing.py

--- URL_Routing.py
+++ URL_Routing.py
@@ -7,3 +7,11 @@ urlpatterns = [
     path('login/', views.login_voter, name='login'),
     path('vote/', views.vote, name='vote'),
 ]
+# online_voting_system/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('voting/', include('voting.urls')),  # Include voting app URLs
+]
